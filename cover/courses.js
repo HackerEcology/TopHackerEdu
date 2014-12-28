6f3f9af393b2c0381241d0868154c461ec8ca35a
Zhang Jian Nan <jn.zhang610@gmail.com>
2014-12-28 17:54:14 +0800

    add angular app, not finished

---
+++ cover/courses.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
+    $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');
+    $('#products .item').addClass('grid-group-item');});
+});
+
+var app = angular.module('IdeaBoard',['akoenig.deckgrid','btford.modal','ngSanitize','ngRoute']);
+
