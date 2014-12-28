e3cfdd20a55070e7e1e74693209f7eec52138a42
tdjackey <tdjackey@gmail.com>
2014-12-28 15:52:02 +0800

    change app.js

--- app.js
+++ app.js
@@ -5,24 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,5 +45,8 @@ app.use(function(err, req, res, next) {
     });
 });
 
-
+if (!module.parent) {
+  app.listen(3001);
+  console.log("Express started at 3001");
+}
 module.exports = app;
